9c8187ced685c39492652573b64eb3ac5ae84d0f
Alex Shillingford <alex.shillingford@icloud.com>
2020-01-06 12:57:35 -0700

    First commit; create Xcode Project inside repository

---
+++ FoodieFun/FoodieFun/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodieFun
+//
+//  Created by Alex Shillingford on 1/6/20.
+//  Copyright © 2020 Alex Shillingford. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
