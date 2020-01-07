79b8153545e868b4aa06c4650d8affb583378953
Alex Shillingford <alex.shillingford@icloud.com>
2020-01-07 14:44:51 -0700

    Wire up IBOutlets for DishDetailViewController

---
+++ FoodieFun/FoodieFun/Storyboards/DishDetailViewController.swift
@@ -0,0 +1,36 @@
+//
+//  DishDetailViewController.swift
+//  FoodieFun
+//
+//  Created by Alex Shillingford on 1/7/20.
+//  Copyright © 2020 Alex Shillingford. All rights reserved.
+//
+
+import UIKit
+
+class DishDetailViewController: UIViewController {
+    
+    @IBOutlet weak var dishDetailImageView: UIImageView!
+    @IBOutlet weak var dishDetailNameLabel: UILabel!
+    @IBOutlet weak var dishDetailRatingLabel: UILabel!
+    @IBOutlet weak var dishDetailCuisineLabel: UILabel!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
