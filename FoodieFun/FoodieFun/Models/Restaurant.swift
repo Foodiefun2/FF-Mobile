a90002adcc53cc59af8657ab3cff6ec681349a1a
Alex Shillingford <alex.shillingford@icloud.com>
2020-01-09 13:45:04 -0700

    Reorganize model files into Model folder

---
+++ FoodieFun/FoodieFun/Models/Restaurant.swift
@@ -0,0 +1,16 @@
+//
+//  Restaurant.swift
+//  FoodieFun
+//
+//  Created by Alex Shillingford on 1/7/20.
+//  Copyright © 2020 Alex Shillingford. All rights reserved.
+//
+
+import Foundation
+
+struct Restaurant {
+    var id: Int?
+    var name, location, hours, review: String
+    var cuisine: Cuisine
+    var image: String
+}
