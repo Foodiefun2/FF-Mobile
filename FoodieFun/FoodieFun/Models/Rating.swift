2114b11f92bc730026f0c6ae8c13b608a22d1e48
Alex Shillingford <alex.shillingford@icloud.com>
2020-01-09 14:32:44 -0700

    Create Rating data model

---
+++ FoodieFun/FoodieFun/Models/Rating.swift
@@ -0,0 +1,15 @@
+//
+//  Rating.swift
+//  FoodieFun
+//
+//  Created by Alex Shillingford on 1/9/20.
+//  Copyright © 2020 Alex Shillingford. All rights reserved.
+//
+
+import Foundation
+
+struct Rating {
+    var id: Int
+    var foodRating, drinksRating, decorRating, serviceRating, cleanlinessRating: String
+    var restaurantID: Int
+}
