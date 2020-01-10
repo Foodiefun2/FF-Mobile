02f3d5159d8d1ee47b306ed8ec37a8f99c37f923
Alex Shillingford <alex.shillingford@icloud.com>
2020-01-09 17:36:22 -0700

    Create logIn() method for authentication

--- FoodieFun/FoodieFun/Model Controllers/NetworkController.swift
+++ FoodieFun/FoodieFun/Model Controllers/NetworkController.swift
@@ -24,16 +24,17 @@ enum NetworkError: Error {
 }
 
 class NetworkController {
-    private let baseURL = URL(string: "https://rayfoodiefun.herokuapp.com/")!
+    private let baseURL = URL(string: "https://rayfoodiefun.herokuapp.com/api")!
+    var bearer: Bearer?
     
     func signUpNewUser(user: User, completion: @escaping (NetworkError?) -> Void) {
         let signUpURL = baseURL
-            .appendingPathComponent("api")
             .appendingPathComponent("auth")
             .appendingPathComponent("register")
         
         var request = URLRequest(url: signUpURL)
         request.httpMethod = HTTPMethod.post.rawValue
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
         
         do {
             let userData = try JSONEncoder().encode(user)
@@ -53,6 +54,54 @@ class NetworkController {
             if let response = response as? HTTPURLResponse,
                 response.statusCode != 200 {
                 NSLog("URL Response Status code is not 200. Status code is: \(response.statusCode)")
+            }
+            
+            completion(nil)
+        }.resume()
+    }
+    
+    func logIn(with user: User, completion: @escaping (NetworkError?) -> Void) {
+        let signInURL = baseURL
+            .appendingPathComponent("auth")
+            .appendingPathComponent("login")
+        
+        var request = URLRequest(url: signInURL)
+        request.httpMethod = HTTPMethod.post.rawValue
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        do {
+            let jsonData = try JSONEncoder().encode(user)
+            request.httpBody = jsonData
+        } catch {
+            NSLog("Error encoding user object: \(error)")
+            completion(.otherError)
+            return
+        }
+        
+        URLSession.shared.dataTask(with: request) { (data, response, error) in
+            if let response = response as? HTTPURLResponse,
+                response.statusCode != 200 {
+                NSLog("Status code not 200. Status code is \(response.statusCode)")
+                completion(.badAuth)
+            }
+            
+            if let error = error {
+                NSLog("Error with URLSession on line \(#line) in \(#file): \(error)")
+                completion(.otherError)
+                return
+            }
+            
+            guard let data = data else {
+                NSLog("Error retreiving data back during logIn URLSession")
+                completion(.badData)
+                return
+            }
+            
+            do {
+                self.bearer = try JSONDecoder().decode(Bearer.self, from: data)
+            } catch {
+                completion(.noDecode)
+                return
             }
             
             completion(nil)
