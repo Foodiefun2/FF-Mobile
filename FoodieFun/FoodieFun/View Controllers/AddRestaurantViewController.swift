98e4d279e8103ac03880145b65eed2ed5644fc0c
Alex Shillingford <alex.shillingford@icloud.com>
2020-01-11 16:00:58 -0700

    Can now reach AddNewRestaurant scene

--- FoodieFun/FoodieFun/View Controllers/AddRestaurantViewController.swift
+++ FoodieFun/FoodieFun/View Controllers/AddRestaurantViewController.swift
@@ -14,6 +14,7 @@ class AddRestaurantViewController: UIViewController {
     @IBOutlet weak var restaurantAddressTextView: UITextView!
     @IBOutlet weak var restaurantHourOpensTextField: UITextField!
     @IBOutlet weak var restaurantHourClosesTextField: UITextField!
+    @IBOutlet weak var cuisineTextField: UITextField!
     @IBOutlet weak var foodRatingTextField: UITextField!
     @IBOutlet weak var drinksRatingTextField: UITextField!
     @IBOutlet weak var serviceRatingTextField: UITextField!
@@ -34,6 +35,40 @@ class AddRestaurantViewController: UIViewController {
     }
     
     @IBAction func saveRestaurantButtonTapped(_ sender: UIButton) {
+        guard let name = restaurantNameTextField.text,
+            !name.isEmpty,
+            let address = restaurantAddressTextView.text,
+            !address.isEmpty,
+            let opens = restaurantHourOpensTextField.text,
+            !opens.isEmpty,
+            let closes = restaurantHourClosesTextField.text,
+            !closes.isEmpty,
+            let cuisine = cuisineTextField.text,
+            !cuisine.isEmpty,
+            let foodRating = foodRatingTextField.text,
+            !foodRating.isEmpty,
+            let drinksRating = drinksRatingTextField.text,
+            !drinksRating.isEmpty,
+            let serviceRating = serviceRatingTextField.text,
+            !serviceRating.isEmpty,
+            let cleanlinessRating = cleanlinessRatingTextField.text,
+            !cleanlinessRating.isEmpty,
+            let decorRating = decorRatingTextField.text,
+            !decorRating.isEmpty,
+            let reviewText = restaurantReviewTextView.text,
+            !reviewText.isEmpty else {
+                let alert = UIAlertController(title: "Incomplete!", message: "Please fill out all the required text fields", preferredStyle: .alert)
+                let alertAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
+                alert.addAction(alertAction)
+                self.present(alert, animated: true, completion: nil)
+                return
+        }
+        
+        let restaurant = Restaurant(id: nil, userID: NetworkController.shared.user?.id, name: name, location: address, hours: "\(opens) - \(closes)", image: nil, cuisine: cuisine, reviews: [])
+        
+        NetworkController.shared.postNewRestaurant(restaurant: restaurant)
+        
+        self.dismiss(animated: true, completion: nil)
     }
     
 
